64615cb0d5cf12800d4529b042751f15bd23a271
Yifei Li 李逸飞 <liyifei723@gmail.com>
2026-03-30 16:34:06 -0400

    remove action rate reward

--- scripts/rsl_rl/train_race.py
+++ scripts/rsl_rl/train_race.py
@@ -112,7 +112,7 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
         'approach_speed_reward_scale': 4.0,
         'exit_speed_reward_scale': 3.0,
         'gate_proximity_reward_scale': 0.5,
-        'action_rate_reward_scale': -0.05,
+        # 'action_rate_reward_scale': -0.05,
         'time_penalty_reward_scale': -0.5,
         'crash_reward_scale': -2.0,
         'death_cost': -50.0,
